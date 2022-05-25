81e7d0164c158e3699a3967a8076ed0a487f50a2
Golf Sinteppadon <golf@figma.com>
2022-05-25 15:20:41 -0400

    basic proximity animation

--- widget-src/proximity_animations.tsx
+++ widget-src/proximity_animations.tsx
@@ -14,12 +14,23 @@ import { isOverlapping } from './lib'
 const FPS = 30
 
 export function proximityAnimations(
-  characterRect: Rect,
+  characterRect: WidgetNode,
   proximityAnimationRects: (FrameNode | GroupNode)[]
 ) {
-  const widgetId = widget.useWidgetId()
+  addOrRemoveAnimations(characterRect, proximityAnimationRects)
+  incrementAnimations()
+}
+
+function addOrRemoveAnimations(
+  characterRect: WidgetNode,
+  proximityAnimationRects: (FrameNode | GroupNode)[]
+) {
+  const widgetId = characterRect.id
   for (const a of proximityAnimationRects) {
     if (isOverlapping(characterRect, a)) {
+      if (!!a.getPluginData('animation')) {
+        break
+      }
       a.setPluginData(
         'animation',
         JSON.stringify({
@@ -28,26 +39,54 @@ export function proximityAnimations(
         })
       )
 
-      if (DEBUG) {
-        console.log(`overlapping with ${a.name}`)
-      }
       for (const c of a.children) {
         c.visible = false
       }
 
-      let i = 0
-      const myInterval = setInterval(() => {
-        if (i < a.children.length) {
-          a.children[i].visible = true
-          i++
-        } else {
-          clearInterval(myInterval)
-        }
-      }, FPS * 10)
+      currentAnimations[a.id] = {
+        framesSinceLast: 0,
+        nextChildIndex: 0,
+        numChildren: a.children.length,
+        animationNode: a
+      }
     } else {
-      const { id, timestamp } = JSON.parse(a.getPluginData('animation'))
+      delete currentAnimations[a.id]
+      a.setPluginData('animation', '')
     }
   }
 }
 
-// create data structure holding all ongoing animations
+interface AnimationState {
+  framesSinceLast: number
+  nextChildIndex: number
+  numChildren: number
+  animationNode: FrameNode | GroupNode
+}
+let currentAnimations: { [nodeId: string]: AnimationState } = {}
+
+function incrementAnimations() {
+  for (const nodeId of Object.keys(currentAnimations)) {
+    const { framesSinceLast, nextChildIndex, numChildren, animationNode } =
+      currentAnimations[nodeId]
+
+    if (DEBUG)
+      console.log(nextChildIndex, ((nextChildIndex + 1) % numChildren) - 1)
+
+    if (framesSinceLast === 0) {
+      animationNode.children[nextChildIndex].visible = false
+      animationNode.children[(nextChildIndex + 1) % numChildren].visible = true
+    }
+
+    if (DEBUG) console.log(currentAnimations[nodeId])
+
+    currentAnimations[nodeId] = {
+      framesSinceLast: (framesSinceLast + 1) % 5,
+      nextChildIndex:
+        framesSinceLast === 0
+          ? (nextChildIndex + 1) % numChildren
+          : nextChildIndex,
+      numChildren,
+      animationNode
+    }
+  }
+}
