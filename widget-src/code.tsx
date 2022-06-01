8824eaaf44900031d584ee0a1db5d85de35125f8
Golf Sinteppadon <golf.sinteppadon@gmail.com>
2022-05-31 22:51:51 -0400

    simplest crash repro

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,92 +1,31 @@
 const { widget } = figma
-const { useSyncedState, usePropertyMenu } = widget
-import { bikeZone } from './bike_zone_🚲'
-import { allCharacters, getSprite } from './img/sprites'
-import { Facing, toRect } from './lib'
+const { useSyncedState } = widget
 const { Image } = widget
-import {
-  movement
-} from './movement_🛑'
-import { animatedArt } from './animated_art_⏱'
-import { midpoint } from './vector'
-import { wardrobe, wardrobePropertyMenuItem } from './wardrobe_🏠'
 
-export const DEBUG = false // Add options to debug the widget
+import guydown0 from './img/guy1/basic-down-0.png'
+import guyup0 from './img/guy1/basic-up-0.png'
 
-const FPS = 30
-
-// How does the char know to alternate steps at all if this is not useSyncedState?
-let lastSpriteIndex = 0
-
-function nextFrame(props: {
-  widgetNode: WidgetNode
-  setFacing: (facing: Facing) => void
-  inWardrobe: boolean
-  setInWardrobe: (inWardrobe: boolean) => void
-}) {
-  const { widgetNode, setFacing, inWardrobe, setInWardrobe } = props
-  const widgetRect = toRect(widgetNode)
-  const nodes = figma.currentPage.findAll(() => true)
-  const animatedArtNodes = nodes.filter(
-    (n) => n.name[0] === '⏱' && ['FRAME', 'GROUP'].includes(n.type)
-  ) as (FrameNode | GroupNode)[]
-  const collisionNodes = nodes.filter((n) => n.name.slice(0, 2) === '🛑')
-  const wardrobeNodes = nodes.filter((n) => n.name.slice(0, 2) === '🏠')
-  const bikeZoneNodes = nodes.filter((n) => n.name.slice(0, 2) === '🚲')
-
-  lastSpriteIndex = movement({
-    widgetNode,
-    widgetRect,
-    setFacing,
-    lastSpriteIndex,
-    collisionNodes
-  })
-  animatedArt(widgetNode.id,toRect(widgetNode), animatedArtNodes)
-  bikeZone(widgetRect, bikeZoneNodes)
-  wardrobe(widgetRect, wardrobeNodes, inWardrobe, setInWardrobe)
-}
+const FPS = 60
 
 function Widget() {
-  // todo: if wardrobeIndex == -1, create a node that spawns widgets?
-  const widgetId = widget.useWidgetId()
-  const [wardrobeIndex, setWardrobeIndex] = useSyncedState<number>(
-    'wardrobeIndex',
-    0
-  )
-  const [inWardrobe, setInWardrobe] = useSyncedState<boolean>(
-    'inWardrobe',
-    false
-  )
-
-  const propertyMenu: WidgetPropertyMenuItem[] = inWardrobe ? [wardrobePropertyMenuItem(wardrobeIndex)] : []
-  usePropertyMenu(propertyMenu, ({ propertyValue }) => {
-    setWardrobeIndex(allCharacters.findIndex((c) => c.name === propertyValue))
-  })
-
-  const [facing, setFacing] = useSyncedState<Facing>('facing', 'down')
+  const [facing, setFacing] = useSyncedState<string>('facing', 'down')
 
   const activate = () => {
-    const widgetNode = figma.getNodeById(widgetId) as WidgetNode
-    if (widgetNode) {
-      figma.currentPage.selection = [widgetNode]
-    }
-
-    figma.viewport.center = midpoint(widgetNode) // update camera
+    let i = 0
     setInterval(() => {
-      nextFrame({ widgetNode, setFacing, inWardrobe, setInWardrobe })
+      setFacing(i % 2 === 0 ? 'up' : 'down')
+      i++
     }, 1000 / FPS)
     return new Promise<void>(() => {})
   }
 
   return (
       <Image
-      onClick={activate}
+        onClick={activate}
         width={64}
         height={64}
-        src={getSprite(facing, lastSpriteIndex, wardrobeIndex)}
+        src={facing === 'down' ? guydown0 : guyup0}
       />
   )
 }
 widget.register(Widget)
-
-// how to respond to selection change
